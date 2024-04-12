fa2b812f60a35a9de6af6e9192a0d6c47e0ed523
Kenneth <kenaguilar.steve@gmail.com>
2024-04-11 20:35:41 -0600

    v1.1.13 update packages

--- CapaPresentacion/Properties/AssemblyInfo.cs
+++ CapaPresentacion/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
 // mediante el carácter '*', como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.7")]
-[assembly: AssemblyFileVersion("1.1.7")]
+[assembly: AssemblyVersion("1.1.13")]
+[assembly: AssemblyFileVersion("1.1.13")]
 
